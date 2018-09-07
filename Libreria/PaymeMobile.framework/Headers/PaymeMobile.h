a84ad358331edfeaab3c27c41b7d8d5bd1a25d7d
Miguel Angel Tomairo <miguel.tomairo@alignet.com>
2018-09-07 09:04:26 -0500

    se corrigieron bugs presentes en la libreria

---
+++ Libreria/PaymeMobile.framework/Headers/PaymeMobile.h
@@ -0,0 +1,24 @@
+//
+//  PaymeApi.h
+//  PaymeApi
+//
+//  Created by Ronald on 6/12/18.
+//  Copyright © 2018 Ronald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PaymeApi.
+FOUNDATION_EXPORT double PaymeMobileVersionNumber;
+
+//! Project version string for PaymeApi.
+FOUNDATION_EXPORT const unsigned char PaymeMobileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymeApi/PublicHeader.h>
+
+#import "CardIO.h"
+#import "DLRadioButton.h"
+#import "SwiftyRSA.h"
+#import "NSData+SHA.h"
+#import "FXPageControl.h"
+#import <CommonCrypto/CommonCrypto.h>
